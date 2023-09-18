0b32571510972d9988e8f5d84efd85c63e61bcb5
Itishree2000 <127124225+Itishree2000@users.noreply.github.com>
2023-09-18 19:52:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,34 @@
 function threeSum(arr, target) {
 // write your code here
-  arr.sort((a, b) => a - b);
-  
-  let closestSum = Infinity; // Initialize with a large value
-  let minDiff = Infinity; // Initialize with a large value
-  
-  for (let i = 0; i < arr.length - 2; i++) {
-    let left = i + 1;
-    let right = arr.length - 1;
-    
-    while (left < right) {
-      const currentSum = arr[i] + arr[left] + arr[right];
-      const currentDiff = Math.abs(currentSum - target);
-      
-      if (currentDiff < minDiff) {
-        // Update the closest sum and minimum difference
-        closestSum = currentSum;
-        minDiff = currentDiff;
-      }
-      
-      if (currentSum < target) {
-        // If the current sum is less than the target, move the left pointer to the right
-        left++;
-      } else {
-        // If the current sum is greater than or equal to the target, move the right pointer to the left
-        right--;
-      }
-    }
-  }
-  
-  return closestSum;
+  //sort the array
+  arr.sort((a,b) =>a-b);
+  let closestSum=infinity;//initialise the largest value
+  let minDiff=infinity;   //initialise the largest value
+  let arr.length=n;
+  for(let i=0;i<n-2;i++)
+	  {
+		let left=i+1;
+		let right=n-1;
+		while(left<right)
+			{
+				const currentSum=arr[i]+arr[left]+arr[right];
+				const currentDiff=math.abs(currrentSum-target);
+				if(currentDiff<currentSum)
+				{
+					closestSum=currentSum;
+					minDiff=currentDiff;
+				}
+				if(currentSum<target)
+				{
+					left++;
+				}
+				else
+				{
+					right--;
+				}
+			}
+	  }
+	return closestSum;
 }
 
 module.exports = threeSum;
